d63edaf73dd5438e9332d554ae337c4a1ebb6e88
christy.binu91@gmail.com <ChristyBinu-4>
2022-11-10 15:12:47 +0530

    lab3

---
+++ ASKR/Question3.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+
+columnName = []
+
+df = pd.read_csv("data.csv")
+print(df.to_string())
+
+columnName = df.keys()
+
+print("\nmax value of each column :")
+for i in columnName:
+    print(df[i].max())
+print("\nmin value of each column : ")
+for i in columnName:
+    print(df[i].min())
+
+
+print("Mean value of each column",df.mean(),sep="\n")
