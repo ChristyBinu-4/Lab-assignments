88eedd616e7881d79da1ed883f9c1b057aebbbea
ChristyBinu-4 <christy.binu91@gmail.com>
2022-12-23 18:20:51 +0530

    23/12/2022 BFS

---
+++ ACO/BFS.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def BFS(graph, startVertex):
+  queue = deque([startVertex])
+  visited = {}
+  traversalList = []
+
+  for i in range(len(graph)):
+    visited.update({i:False})
+
+  while queue:
+    vertex = queue.popleft()
+    visited[vertex] = True
+    traversalList.append(vertex)
+
+    for node in graph[vertex]:
+      if visited[node] is False and node not in queue:
+        queue.append(node)
+
+  return traversalList
+
+graph = { 0:[1, 2, 3], 1:[0, 2], 2:[0, 1, 4], 3:[0], 4:[2]}
+print(BFS(graph,0))
