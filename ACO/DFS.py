cd2e304ccc467edf2fb6f6e7e2b0e96179f14974
ChristyBinu-4 <christy.binu91@gmail.com>
2022-12-24 12:20:20 +0530

    24/12/2022 DFS

---
+++ ACO/DFS.py
@@ -0,0 +1,23 @@
+def DFS(graph, startVertex):
+  queue = [startVertex]
+  visited = {}
+  traversalList = []
+
+  for i in range(len(graph)):
+    visited.update({i:False})
+
+  while queue:
+    vertex = queue.pop(0)
+    visited[vertex] = True
+    traversalList.append(vertex)
+    nodeIndex = 0 # It is used to avoid reversing of nodes in graph[vertex]
+
+    for node in graph[vertex]:
+      if visited[node] is False and node not in queue: 
+        queue.insert(nodeIndex,node)
+        nodeIndex += 1
+
+  return traversalList
+
+graph = { 0:[1, 2, 3], 1:[0, 2], 2:[0, 1, 4], 3:[0], 4:[2]}
+print(DFS(graph,0))
