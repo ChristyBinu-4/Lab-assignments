1d8b36b45bfef38005c43607cf95aeb5e5d567c7
ChristyBinu-4 <christy.binu91@gmail.com>
2023-01-23 19:10:17 +0530

    File reorder

---
+++ mathematics/Set 1/Set1-8.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy.linalg import norm
+from math import inf
+
+print("Enter the values of  vector : ")
+list1 = [int(input()) for i in range(3)]
+    
+vector1 = np.array(list1)
+
+resultVector1 = norm(list1, 1)
+resultVector2 = norm(list1, 2)
+resultVector3 = norm(list1, inf)
+
+print("The L1 norm of vector is : ", resultVector1)
+print("The L2 norm of vector is : ", resultVector2)
+print("The Max norm of vector is : ", resultVector3)
