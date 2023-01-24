bae3402ac26dbc0af9b809ad8cb43eb8e9f40c8f
Christy Binu <106164070+ChristyBinu-4@users.noreply.github.com>
2023-01-24 10:24:24 +0000

    Stock Market data to candle

--- ASKR/stockquestion1.py
+++ ASKR/stockquestion1.py
@@ -1,46 +1,20 @@
-import plotly.graph_objects as go
-from numpy import Inf
 import pandas as pd
 
-def getMean(column):
-    sum = 0 
-    for i in column:
-        sum += i
-    mean = sum / len(column)
-    return mean
+df = pd.read_csv('chart (1).csv', index_col=0, parse_dates=True)
 
-def getMin(column):
-    min = Inf
-    for i in column:
-        if i < min :
-            min = i
-    return min
 
-def getMax(column):
-    max = 0 
-    for i in column:
-        if i > max :
-            max = i
-    return max
+df = df.drop('Pre Open RELIANCE', axis=1)
+df = df.dropna()
 
-df = pd.read_csv('chart.csv')
+dfResample = df.resample('15min').ohlc()
 
-time = df.columns[0]
-stock = df.columns[1]
+# ohlc = {
+#     'open' : 'first',
+#     'high' : 'max',
+#     'low'  : 'min',
+#     'close': 'last',
+#     'volume': 'sum'
+# }
 
-oneMinutePrice = dict()
-
-#Entering all data values into a dictionary
-for index, datetime in enumerate(df[time]):
-  if(datetime not in oneMinutePrice.keys()):
-    oneMinutePrice.update({datetime : [df[stock][index]]})
-  else: 
-    oneMinutePrice[datetime].append(df[stock][index])
-
-#finding mean of prices in specfic minutes
-for key in oneMinutePrice:
-  meanOfKey = getMean(oneMinutePrice[key])
-  minOfKey = getMin(oneMinutePrice[key])
-  maxOfKey = getMax(oneMinutePrice[key])
-  oneMinutePrice.update({key : [meanOfKey, minOfKey, maxOfKey]})
-  print(key , " = > ", oneMinutePrice[key])
+# dfResample = df.resample('5min', base=15).apply(ohlc)
+print(dfResample)
