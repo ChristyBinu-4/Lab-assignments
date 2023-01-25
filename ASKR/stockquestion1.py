09e042f14afed4b394dec7b6946dd3c9b9698bac
ChristyBinu-4 <christy.binu91@gmail.com>
2023-01-25 14:44:58 +0530

    25/01/2023 stock  changes

--- ASKR/stockquestion1.py
+++ ASKR/stockquestion1.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv('chart (1).csv', index_col=0, parse_dates=True)
+df = pd.read_csv('chart.csv', index_col=0, parse_dates=True)
 
 
-df = df.drop('Pre Open RELIANCE', axis=1)
+df = df.drop('Pre Open TATAMOTORS', axis=1)
 df = df.dropna()
 
-dfResample = df.resample('15min').ohlc()
+dfResample = df.resample('5min').ohlc()
 
 print(dfResample)
+MaximumValue = dfResample[('TATAMOTORSEQN', 'high')].max()
+MinimumValue = dfResample[('TATAMOTORSEQN', 'low')].min()
+
+print("The maximum value of stock is : ", MaximumValue)
+print("The minimum value of stock is : ", MinimumValue)
