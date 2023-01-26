3404e41b72e4b9a0b97cf49045eaa1a09c7c17df
ChristyBinu-4 <christy.binu91@gmail.com>
2023-01-26 09:32:53 +0530

    26/01/2023 Stock import file changes

--- ASKR/stockquestion1.py
+++ ASKR/stockquestion1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('chart.csv', index_col=0, parse_dates=True)
-
+df = pd.read_csv("https://raw.githubusercontent.com/ChristyBinu-4/Lab-assignments/main/ASKR/chart.csv", 
+                index_col = 0, parse_dates = True)
 
 df = df.drop('Pre Open TATAMOTORS', axis=1)
 df = df.dropna()
