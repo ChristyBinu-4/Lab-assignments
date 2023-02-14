8c7bd40498545dff735c924be866df1a666e5360
ChristyBinu-4 <christy.binu91@gmail.com>
2023-02-14 15:05:33 +0530

    Changes with submission document(maths lab)

--- mathematics/Set 1/Set1-7.py
+++ mathematics/Set 1/Set1-7.py
@@ -4,8 +4,8 @@ import numpy as np
 print("Enter the  values of first vector : ")
 list1 = [int(input()) for i in range(3)]
 
-scalar = int(input("Enter the scalar value"))
-vector1 = np.array(list)
+scalar = int(input("Enter the scalar value : "))
+vector1 = np.array(list1)
 
 resultVector = vector1 * scalar
 
