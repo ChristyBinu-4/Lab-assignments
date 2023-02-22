599577ac2b03e7c905c4d54b5c4d45de8fd0b7cc
ChristyBinu-4 <christy.binu91@gmail.com>
2023-02-22 15:34:43 +0530

    22/02/2023 Iterative deepening search

--- Lab-Cycle/Cycle 2/Cycle2-3.py
+++ Lab-Cycle/Cycle 2/Cycle2-3.py
@@ -1,2 +1,30 @@
-#Question 8: Iterative deepening depth first search
+#Question 9: Iterative deepening depth first search
 
+
+def DFS(graph, currentNode, depth):
+    print(currentNode, end="->")
+    if depth > 0:
+        for node in graph[currentNode]:
+            if DFS(graph, node, depth - 1):
+                return True
+    else:
+        return False
+
+
+def IDDFS(graph, currentNode, depth):
+    for i in range(depth):
+        if DFS(graph, currentNode, i):
+            return True
+    return False
+
+
+graph = {
+  'A' : ['B', 'C'],
+  'B' : ['D', 'E'],
+  'C' : ['G'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : [],
+  'G' : []
+}
+IDDFS(graph, 'A', 4)
