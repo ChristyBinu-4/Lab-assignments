68261999de35739d04328e94878366d62cdccee4
ChristyBinu-4 <christy.binu91@gmail.com>
2023-02-26 14:39:39 +0530

    26/02/2023 UCS testing 1 completed ->Testing2

--- Lab-Cycle/Cycle 2/Cycle2-4.py
+++ Lab-Cycle/Cycle 2/Cycle2-4.py
@@ -22,49 +22,52 @@ def uniformCostSearch(graph, cost, start, goal):
     new_nodes = graph[nameOfselected_node]
 
     if new_nodes:
-  
+    
       for child in new_nodes:
         costToChild = cost[(nameOfselected_node, child)]
-
-        if child not in (opened and closed):
-          opened.append((costToChild + costOfselected_node, child))
+  
+        for i in range(len(opened)):
+          if child in opened[i] :
+              if costToChild < opened[i][0]:
+                print("fuck it")
+                opened.pop(i)
+                opened.append((costToChild, child))
+          if child in closed[i]:
+            break
+        else:
+            opened.append((costToChild + costOfselected_node, child))
         
-        elif child in opened:
-          for i in range(len(opened)):
-            if child == opened[i]:
-              opened.append((costToChild, child))
 
    
     
 
 # create the graph
-cost = {}
+
 
 # add edge
 graph = {
-    'S' : [1, 3],
-    1 : ['G'],
-    2 : [1],
-    3 : [1, 'G', 4],
-    4 : [2, 5], 
-    5 : ['G', 2],
-    'G' : [4]
+    'V1' : ['V2', 'V3'],
+    'V2':  ['V3', 'V4', 'V5'],
+    'V3' : ['V4', 'V5'],
+    'V4' : ['V5', 'V6'],
+    'V5' : ['V6'], 
+    'V6' : []
 }
 # add the cost
-cost[('S', 1)] = 2
-cost[('S', 3)] = 5
-cost[(1, 'G')] = 1
-cost[(3, 1)] = 5
-cost[(3, 'G')] = 6
-cost[(3, 4)] = 2
-cost[(2, 1)] = 4
-cost[(4, 2)] = 4
-cost[(4, 5)] = 3
-cost[(5, 2)] = 6
-cost[(5, 'G')] = 3
-cost[('G', 4)] = 7
+cost = {
+  ('V1', 'V2') : 9,
+  ('V1', 'V3') : 4,
+  ('V2', 'V3') : 2,
+  ('V2', 'V4') : 7,
+  ('V2', 'V5') : 3,
+  ('V3', 'V4') : 1,
+  ('V3', 'V5') : 6,
+  ('V4', 'V5') : 4,
+  ('V4', 'V6') : 8,
+  ('V5', 'V6') : 2,
+}
 
-ucs = uniformCostSearch(graph, cost, start='S', goal='G')
+ucs = uniformCostSearch(graph, cost, start='V1', goal='V6')
 
 if ucs:
   print("Goal node found....", "pathway :", sep="\n")
