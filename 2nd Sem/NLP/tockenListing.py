28d8dda99647d9464f4345b77e1d94f8ed11c036
ChristyBinu-4 <christy.binu91@gmail.com>
2023-03-16 19:56:54 +0530

    16/03/2023 NLP 2nd assignment completed

--- 2nd Sem/NLP/tockenListing.py
+++ 2nd Sem/NLP/tockenListing.py
@@ -1,1 +1,20 @@
-from nltk
+from nltk import word_tokenize
+
+text = open(r"C:\Users\91703\Documents\PG docs\Lab-assignments\2nd Sem\NLP\aliceInWonderLand.txt")
+textString = ""
+
+for i in text:
+  textString += i
+
+text_tokenized = word_tokenize(textString)
+
+token_list = {}
+
+for i in text_tokenized:
+  if i not in token_list:
+    token_list[i] = 1
+  else :
+    token_list[i] += 1
+
+for word in token_list:
+  print('{:<13}  {:<12} '.format(word, token_list[word]))
