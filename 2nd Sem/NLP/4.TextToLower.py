93c7f43411cd31177ffb99f463eb2bbb278ea53c
ChristyBinu-4 <christy.binu91@gmail.com>
2023-03-28 14:06:07 +0530

    28/03/2023 change in question 4

--- 2nd Sem/NLP/4.TextToLower.py
+++ 2nd Sem/NLP/4.TextToLower.py
@@ -1,13 +1,7 @@
-from nltk import word_tokenize
-
 text = open(r"2nd Sem\NLP\aliceInWonderLand.txt")
 text = text.read()
 
-tokenized_text = word_tokenize(text)
-textString = ""
-
-for token in tokenized_text:
-    textString += token.lower() + " "
+textString = text.lower()
 
 print("Input text :", text, sep="\n",end="\n\n")
 print("Converted Lower case text:", textString, sep="\n", end='\n\n')
