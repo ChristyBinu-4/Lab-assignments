24f3807fd21396b273719a57ee089bf2b2aa0b1e
ChristyBinu-4 <christy.binu91@gmail.com>
2023-03-31 23:08:33 +0530

    31/03/2023 Qn.12 Onehotencoder completed

---
+++ 2nd Sem/NLP/12.oneHotEncoder.py
@@ -0,0 +1,32 @@
+from nltk import word_tokenize
+
+text = 'this is a very good book to study'
+print("Input text: ", text, "\n")
+
+data = word_tokenize(text)
+
+encoded = {}
+count = -1
+
+for i in data:
+    if i not in encoded:
+        count += 1
+        encoded[i] = count
+    else:
+        continue
+    
+oneHotEncoding = []
+print("One Hot encoding of text")
+
+
+for i in data:
+    array = []
+    for j in encoded:
+        if i == j:
+            array.append(1)
+        else:
+            array.append(0)
+    oneHotEncoding.append(array)
+
+for i,j  in zip(oneHotEncoding,data):
+    print(f'{j}\t{i}')
