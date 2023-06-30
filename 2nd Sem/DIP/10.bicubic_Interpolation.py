23cf04443a8ad2c0cd7fc50c600deaf972763f3f
ChristyBinu-4 <christy.binu91@gmail.com>
2023-07-01 00:31:47 +0530

    DIP interpolation completed

---
+++ 2nd Sem/DIP/10.bicubic_Interpolation.py
@@ -0,0 +1,28 @@
+#Bicubic Interpolation
+import cv2 
+import numpy as np
+import matplotlib.pyplot as plt
+from math import sqrt, floor 
+import math
+
+
+
+
+img = cv2.imread("2nd Sem/DIP/butterfly.png")
+size = img.shape
+row = size[0]
+column = size[1]
+
+
+enlargeScale = 2
+dimension = (row*enlargeScale, column*enlargeScale)
+
+new_image = cv2.resize(img, dimension, interpolation=cv2.INTER_NEAREST)
+
+
+cv2.imshow("original",img)
+cv2.waitKey(10000)
+
+
+cv2.imshow("NNI", new_image)
+cv2.waitKey(10000)
