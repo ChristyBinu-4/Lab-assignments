5bebd08651c27c3b11317817ad899c33cf3b6ba2
ChristyBinu-4 <christy.binu91@gmail.com>
2023-07-24 16:20:32 +0530

    cycle 2

---
+++ 2nd Sem/TFML/python/Cycle-iii/2.SVMmajorityvoting.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import urllib.request
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import VotingClassifier
+
+data = pd.read_csv('2nd Sem\TFML\Datasets\iris.csv', header=None)
+data = data.drop()
+
+x = data.iloc[:, :-1]  
+y = data.iloc[:, -1] 
+
+
+
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# svm_linear = SVC(kernel='linear')
+# svm_rbf = SVC(kernel='rbf')
+# svm_poly = SVC(kernel='poly')
+
+# voting_model = VotingClassifier(estimators=[('svm_linear', svm_linear), ('svm_rbf', svm_rbf), ('svm_poly', svm_poly)], voting='hard')
+
+# voting_model.fit(x_train, y_train)
+
+# #predict the labels for test data
+# y_pred = voting_model.predict(x_test)
+
+# accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)
+
+# # Print the accuracy
+# print("Accuracy:", accuracy)
+
