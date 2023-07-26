c2158658b1e13b1dbe6d05737bb287eeaf6afec7
ChristyBinu-4 <christy.binu91@gmail.com>
2023-07-26 14:57:02 +0530

    second last program of TFML completed

--- 2nd Sem/TFML/python/Cycle-iii/3.DimensionalityReduction.py
+++ 2nd Sem/TFML/python/Cycle-iii/3.DimensionalityReduction.py
@@ -10,8 +10,25 @@ x = data.drop(data.columns[0], axis=1)
 y = data[data.columns[0]]
 
 pca_dimensions = [300, 400, 500]
+result = []
 
 for dimensions in pca_dimensions:
 
     pca = PCA(n_components=dimensions)
-    
+    x_pca = pca.fit_transform(x)
+
+    x_train, x_test, y_train, y_test = train_test_split(x_pca, y, test_size=0.2, random_state=42)
+    svm = SVC()
+    svm.fit(x_train, y_train)
+    y_pred = svm.predict(x_test)
+
+    precision = precision_score(y_test, y_pred, average='macro', zero_division=1)
+    recall = recall_score(y_test, y_pred, average='macro', zero_division=1)
+    f1 = f1_score(y_test, y_pred, average='macro', zero_division=1)
+
+    result.append({'Dimension': dimensions, 'Precision' : precision, 'Recall' : recall, 'F1 score': f1})
+
+result = pd.DataFrame(result) #making result as a dataframe for printing
+
+print(result)
+
